b6909a8b79be8773de7364ed4ea409d67ff27a84
Joar Classon <joarc@joarc.se>
2016-08-27 17:15:14 +0200

    Finished playbtn loading from firebase db

--- public/js/playbtn.js
+++ public/js/playbtn.js
@@ -1,29 +1,27 @@
-var playbtn; // Stoppa in med ajax
+$(document).on("ready", function(){
+  database.ref("elements").once("value").then(function(data){
+    console.log("playbtn", data.val());
+    var playbtn = data.val();
+    var allelms = [];
+    $(".tab1 td").each(function(i,o){
+    if (!$(this).hasClass("td-extend") &&
+        !$(this).hasClass("td-header") &&
+        !$(this).hasClass("td-none") &&
+        !$(this).hasClass("td-about") &&
+        !$(this).hasClass("td-logo")) {
 
-database.ref("elements").once("value").then(function(data){
-
-});
-
-$(document).on('ready', function(e) {
-  var allelms = [];
-  $(".tab1 td").each(function(i,o){
-  if (!$(this).hasClass("td-extend") &&
-      !$(this).hasClass("td-header") &&
-      !$(this).hasClass("td-none") &&
-      !$(this).hasClass("td-about") &&
-      !$(this).hasClass("td-logo")) {
-
-      var nr = $(this).find(".atomic_number").text();
-      var name = $(this).find(".atomic_text").text();
-      $(this).attr("data-number", nr);
-      $(this).attr("data-name", name);
-      $(this).attr("data-iselm", "true");
-      allelms.push(name);
+        var nr = $(this).find(".atomic_number").text();
+        var name = $(this).find(".atomic_text").text();
+        $(this).attr("data-number", nr);
+        $(this).attr("data-name", name);
+        $(this).attr("data-iselm", "true");
+        allelms.push(name);
+      }
+    });
+    for (var key in playbtn) {
+      if (!playbtn.hasOwnProperty(key)) continue;
+      var obj = playbtn[key];
+      $('td[data-name="'+obj.element+'"]').find(".atomic_number").after('<img class="yt" src="/img/play.svg">');
     }
   });
-  for (var key in playbtn) {
-    if (!playbtn.hasOwnProperty(key)) continue;
-    var obj = playbtn[key];
-    $('td[data-name="'+obj.element+'"]').find(".atomic_number").after('<img class="yt" src="/img/play.svg">');
-  }
 });
